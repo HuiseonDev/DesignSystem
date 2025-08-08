75ef741f048b8b386783a12e06a0be2034f8337e
전희선 <designh2sun@gmail.com>
2025-08-08 16:47:08 +0900

    Style: GlobalStyle, Break points, common, Variables 생성

---
+++ src/styles/BreakPoint.tsx
@@ -0,0 +1,9 @@
+export const mqMin = (point: string) => `@media (min-width: ${point})`;
+export const mqMax = (point: string) => `@media (max-width: ${point})`;
+
+export const breakPoints = {
+  pc: "1024px",
+  moMax: "1023px",
+  moMid: "768px",
+  moSm: "499px",
+};
