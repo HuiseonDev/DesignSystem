8ba917edcd034d97f9da123490632d88c8a1e742
전희선 <designh2sun@gmail.com>
2025-08-08 16:45:09 +0900

    Chore: 라우트 설정 및 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Global, ThemeProvider } from "@emotion/react";
+import variables from "./styles/Variables";
+import GlobalStyles from "./styles/GlobalStyles";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "./components/Error/Error";
+
 function App() {
-  return <div>디자인 시스템</div>;
+  return (
+    <ThemeProvider theme={variables}>
+      <Global styles={GlobalStyles} />
+      <ErrorBoundary FallbackComponent={Error}>
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </ThemeProvider>
+  );
 }
 
 export default App;
